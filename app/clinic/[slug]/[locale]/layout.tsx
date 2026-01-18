57e46f1f2dec80accc74d0e209858a9eddb2fb50
大熊 葵月 <information.dentee@gmail.com>
2026-01-18 22:37:58 +0900

    Remove: 古い[slug]フォルダを削除

--- app/clinic/[slug]/[locale]/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function ClinicLocaleLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <>{children}</>;
-}
--- app/clinic/[slug]/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function ClinicLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <>{children}</>;
-}
